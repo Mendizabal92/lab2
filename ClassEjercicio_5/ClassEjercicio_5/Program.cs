afe0e466dcf6675f537f3c03acacb3c0f9d359be
julian <jmendi.92@hotmail.com>
2016-03-23 00:12:23 -0300

    exercise5

---
+++ ClassEjercicio_5/ClassEjercicio_5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassEjercicio_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro 5";/*cual era la instruccion que dentro del for no lo tenga qe esta pasando uno por uno*/
+                                              
+
+            int numero;
+            int acumulador = 0;
+            int acumuladorj = 0;
+
+            Console.WriteLine("Ingrese un numero: ");
+            numero = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < numero; i++)
+            {
+                acumulador += i;   
+            }
+            for (int j = numero+1; j < 999 ; j--)
+            {
+                if (acumulador==acumuladorj)
+                {
+                    Console.WriteLine("{0}",j);
+                    break;
+                }
+                else
+                {
+                    acumuladorj += j;
+                }
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
